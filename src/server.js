57b9054e0efbb5d2ec279a8f34aad4638f419b62
Adrian C. Manatad <89061206+lladrian@users.noreply.github.com>
2025-05-13 10:55:30 +0800

    updated

--- src/server.js
+++ src/server.js
@@ -12,6 +12,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.json({ message: "Server is running!" });
+});
 
 
 app.post('/add_message', async (req, res) => {    
