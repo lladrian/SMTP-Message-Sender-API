9a21b1dcd09ebc244158142af482e0717a3ac0b7
Adrian C. Manatad <89061206+lladrian@users.noreply.github.com>
2025-05-17 14:36:01 +0800

    updated

--- src/controllers/visitor_controller.js
+++ src/controllers/visitor_controller.js
@@ -47,9 +47,9 @@ export const visit_page = asyncHandler(async (req, res) => {
             //const diffMs = now - lastVisitTime;
             //const diffMinutes = diffMs / (1000 * 60);
 
-            if (diffMinutes < 1) {
-                return res.status(200).json({ message: 'Duplicate entry within 10 minutes.' });
-            }
+            // if (diffMinutes < 1) {
+            //     return res.status(200).json({ message: 'Duplicate entry within 10 minutes.' });
+            // }
         }
 
         await newVisitor.save();
